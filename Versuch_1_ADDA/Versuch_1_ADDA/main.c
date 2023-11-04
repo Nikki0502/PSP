e794ea629cc9c3493899bf33860e2ba962f87bba
nikumbach <nikumbach@gmail.com>
2023-11-04 19:57:51 +0100

    added switch case for choosing the function

--- Versuch_1_ADDA/Versuch_1_ADDA/main.c
+++ Versuch_1_ADDA/Versuch_1_ADDA/main.c
@@ -1,9 +1,20 @@
 #include <avr/io.h>
+#include <stdio.h>
 #include "os_input.h"
+#include "adda.h"
 
 int main(void) {
     os_initInput();
-	manuell();
+	uint8_t var = 1;// "Es genügt, das zu startende Unterprogramm im Sourcecode festlegen zu koennen"
+	switch (var){
+		case 1:
+			manuell();
+		case 2:
+			tracking();
+		case 3:	
+			sar();
+	}
+	return 0;
 }
 
 
