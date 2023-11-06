adcc31684f29de2dde0b7f1d7be8c95908c8f6a5
nikumbach <nikumbach@gmail.com>
2023-11-06 22:15:29 +0100

    fixed shit adda

--- Versuch_1_ADDA/Versuch_1_ADDA/adda.c
+++ Versuch_1_ADDA/Versuch_1_ADDA/adda.c
@@ -11,15 +11,18 @@ void initLEDandR2R(void){
 	PORTD |= 0b11111111;//Pull-Up for DIP Input
 	DDRA |= 0b11111111;//LEDS as Output
 	DDRB |= 0b11111111;//R-2R-Network as Output
+	PORTA &= 0b00000000;//Leds out
+	PORTB &= 0b00000000;//R-2R out
 }
 
 
 /*Digital to Analog R-2R-Network*/
 void manuell(void){
 	while(true){ //FRAGE ZU kontinuierlriche uebertragung!!!!!!!!!!!!!!!!!!!
+		os_waitForInput(); // C1 pressed
 		PORTA &= 0b00000000;//reset LEDS
 		PORTB &= 0b00000000;//reset R-2R-Network
-		uint8_t pinstate = ~PIND;//current DIP Switches
+		uint8_t pinstate = ~(PIND);//current DIP Switches
 		PORTA |= pinstate;//matching Leds Output
 		PORTB |= pinstate;//matching R-2R Output	
 	}
@@ -28,14 +31,14 @@ void manuell(void){
 void tracking(void){
 	while(true){
 		os_waitForInput(); // C1 pressed
-		uint8_t ref = 0b10000000;//	"Variable ref festgehalten, welche zu Beginn mit einem beliebigen Wert initialisiert werden kann." so i choose the middle
+		uint8_t ref = 0b10000000;//	"Variable ref festgehalten, welche zu Beginn mit einem beliebigen Wert initialisiert werden kann." should be most efficient
 		PORTB |= ref; 
 		PORTA |= ref; // display changes with LEDS
 		_delay_ms(50);// wait for change to affect the comparator 
-		bool begining = (PINC0 == 1); // true if Uref>Umess false otherwise
+		bool begining = ((PINC&0b00000001) == 1); // true if Uref>Umess false otherwise
 		bool current = begining; 
 		while(begining == current){
-			if(current== true){
+			if(current== true){// true if Uref>Umess false otherwise
 				ref = ref - 1 ;
 			}
 			else{
@@ -46,7 +49,8 @@ void tracking(void){
 			PORTA |= ref; // display changes with LEDS
 			PORTA &= ref; // display changes with LEDS
 			_delay_ms(50);// wait for change to affect the comparator 
-			current = (PINC0 == 1);
+			current = ((PINC&0b00000001) == 1);
+			if(ref==0b0000000 || ref ==0b11111111){break;}
 		}
 	}
 	
@@ -63,7 +67,7 @@ void sar(void){
 			PORTB |= ref;
 			PORTA |= ref;// display changes with LEDS
 			_delay_ms(50);// wait for change to affect the comparator 
-			if(PINC0 == 1){ // testing if Uref is larger than Umess 
+			if((PINC&0b00000001) == 1){ // testing if Uref is larger than Umess 
 				ref &=~(1<<i);
 				PORTB &= ref;
 				PORTA &= ref;// display changes with LEDS
